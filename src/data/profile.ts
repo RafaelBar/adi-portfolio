a62370b8cc68d566079d7b7584b4a9b8cc2134db
Rafael <rafael@dreamgroup.com>
2026-06-21 19:58:31 +0300

    Update Facebook link in profile data to a specific user page

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -5,7 +5,7 @@ export const profile: Profile = {
   phone: '+972528823790',
   portrait: '/primary-image.png',
   instagram: 'https://www.instagram.com/badassprints_/',
-  facebook: 'https://www.facebook.com/AdiBadasss',
+  facebook: 'https://www.facebook.com/profile.php?id=61556188187674',
 }
 
 export function whatsappUrl(phone: string): string {
